2de4e41abe6a697f1b6f79b7f97248436092e8b4
Andres Wellmann <wellmann39@gmail.com>
2021-03-18 13:29:35 -0600

    Docs agregado

---
+++ docs/precache-manifest.3b05bc473b789fae4bbaa4e1c6c014fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5760b07298a5607db3e9e38be831b404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9928ae48ba7c0444a03c",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "ade4d16084942723b7ae",
+    "url": "/static/js/2.9ed117c5.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.9ed117c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9928ae48ba7c0444a03c",
+    "url": "/static/js/main.23de2306.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b08a7ca4ec68286c02b7c9ab1776e5db",
+    "url": "/static/media/AndresW.b08a7ca4.jpeg"
+  },
+  {
+    "revision": "625b1babc7c6c47e8c56b0b3d17a162d",
+    "url": "/static/media/JournalApp.625b1bab.jpeg"
+  },
+  {
+    "revision": "850bdd8347c4e20da462f328a358ce7f",
+    "url": "/static/media/Portafolio.850bdd83.jpeg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
